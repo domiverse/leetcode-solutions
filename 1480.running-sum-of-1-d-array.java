f97ed26cdf9bbc53fdd7a6f1ab3fc061d3b50916
Trung Thái <trungthai230700@gmail.com>
2025-07-01 09:58:19 +0700

    solved 1480.running-sum-of-1-d-array

---
+++ 1480.running-sum-of-1-d-array.java
@@ -0,0 +1,20 @@
+
+import java.awt.font.NumericShaper;
+
+/*
+ * @lc app=leetcode id=1480 lang=java
+ *
+ * [1480] Running Sum of 1d Array
+ */
+
+// @lc code=start
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i-1] + nums[i];
+        }
+        return nums;
+    }
+}
+// @lc code=end
+
