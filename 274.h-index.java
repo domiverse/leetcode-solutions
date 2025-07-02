038372e6e9d08a7238e84cb2df6989707ef60193
Trung Thái <trungthai230700@gmail.com>
2025-07-02 11:05:47 +0700

    solved 274.h-index

---
+++ 274.h-index.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=274 lang=java
+ *
+ * [274] H-Index
+ */
+
+// @lc code=start
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for(int i = 0; i< citations.length ; i++){
+            if(citations[i] >=citations.length-i){
+                return citations.length-i;
+            }
+        }
+        return 0;
+    }
+}
+// @lc code=end
+
