e275f5f6ae8ca8b17dc8354fdfec640f78ab9e4e
Trung Thái <trungthai230700@gmail.com>
2025-07-05 04:54:11 +0700

    solved 26.remove-duplicates-from-sorted-array 2.0

--- 26.remove-duplicates-from-sorted-array.java
+++ 26.remove-duplicates-from-sorted-array.java
@@ -10,14 +10,20 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int index = 0;
-        for(int i = 1; i< nums.length; i++){
-            if(nums[index] != nums[i]){
-                index++;
-                nums[index] = nums[i];
+        if (nums.length == 0) {
+            return 0; // Trả về 0 nếu mảng rỗng
+        }
+
+        int uniqueCount = 1; // Đếm số lượng phần tử duy nhất, bắt đầu từ 1 vì phần tử đầu tiên luôn duy nhất
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[uniqueCount - 1]) { // So sánh với phần tử cuối cùng đã duy nhất
+                nums[uniqueCount] = nums[i]; // Cập nhật phần tử duy nhất tiếp theo
+                uniqueCount++; // Tăng số lượng phần tử duy nhất
             }
         }
-        return index + 1;
+
+        return uniqueCount; // Trả về số lượng phần tử duy nhất
     }
 }
 // @lc code=end
