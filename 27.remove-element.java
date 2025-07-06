dc4361b7d5887f4ee3489fc66a3d7ccd75e070fc
Trung Thái <trungthai230700@gmail.com>
2025-07-06 10:12:53 +0700

    solved 27.remove-element 2.0 '

--- 27.remove-element.java
+++ 27.remove-element.java
@@ -10,14 +10,16 @@ import java.util.HashMap;
 // @lc code=start
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index = 0;
+        int soBatDau = 0; // Biến để đếm số lượng phần tử khác giá trị val
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] != val){
-                nums[index] = nums[i];
-                index++;
+            if(nums[i] != val){ // Nếu phần tử hiện tại không bằng val
+                // Đặt phần tử khác val vào vị trí soBatDau
+                nums[soBatDau] = nums[i]; // Cập nhật mảng tại vị trí soBatDau
+                // Tăng biến soBatDau để chỉ đến vị trí tiếp theo
+                soBatDau++;// Tăng số lượng phần tử khác val
             }
-        }        
-        return index;
+        }
+        return soBatDau;
     }
 }
 // @lc code=end
