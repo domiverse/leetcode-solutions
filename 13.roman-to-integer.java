65e32358be1087cc402f9e1eb20cffa81fc094e6
Trung Thái <trungthai230700@gmail.com>
2025-07-09 08:54:17 +0700

    solved 13.roman-to-integer

---
+++ 13.roman-to-integer.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=13 lang=java
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int currValue;
+
+            switch (c) {
+                case 'I':
+                    currValue = 1;
+                    break;
+                case 'V':
+                    currValue = 5;
+                    break;
+                case 'X':
+                    currValue = 10;
+                    break;
+                case 'L':
+                    currValue = 50;
+                    break;
+                case 'C':
+                    currValue = 100;
+                    break;
+                case 'D':
+                    currValue = 500;
+                    break;
+                case 'M':
+                    currValue = 1000;
+                    break;
+                default:
+                    currValue = 0;
+            }
+
+            if (currValue < prevValue) {
+                total -= currValue;
+            } else {
+                total += currValue;
+            }
+            prevValue = currValue;
+        }
+
+        return total;
+    }
+}
+// @lc code=end
+
