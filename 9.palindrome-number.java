0920bdb4ef302265b5605a28842ed8c703f83885
Trung Thái <trungthai230700@gmail.com>
2025-07-12 17:31:38 +0700

    solved [9] Palindrome Number

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -7,20 +7,18 @@
 // @lc code=start
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if(x < 0){
             return false; // Số âm không thể là palindrome
         }
-        int original = x;
-        int reversed = 0;
-
-        while (x > 0) {
-            int digit = x % 10; // Lấy chữ số cuối cùng của x
-            reversed = reversed * 10 + digit; // Thêm chữ số vào reversed
-            x /= 10; // Loại bỏ chữ số cuối cùng khỏi x
+        int nguyenBan = x;
+        int daoNguoc = 0;
+        
+        while(x > 0){
+            int donVi = x % 10; // Lấy chữ số cuối cùng của x
+            daoNguoc = daoNguoc * 10 + donVi; // Thêm chữ số vào daoNguoc
+            x = x / 10; // Loại bỏ chữ số cuối cùng khỏi x
         }
-
-        return original == reversed; // Kiểm tra xem số gốc có bằng số đảo ngược không
+        return nguyenBan == daoNguoc; // Kiểm tra xem số gốc có bằng số đảo ngược không
     }
 }
 // @lc code=end
-
