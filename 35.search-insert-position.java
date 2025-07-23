300e13ea5508f6609415184c9a0a9dae95280c9b
Trung Thái <trungthai230700@gmail.com>
2025-07-23 19:15:10 +0700

    solved 35.search-insert-position

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -7,22 +7,19 @@
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int left = 0; // Khởi tạo chỉ số trái
+        int right = nums.length - 1; // Khởi tạo chỉ số phải
+        while (left <= right) { // Khi chỉ số trái không vượt quá chỉ số phải
+            int mid = left + (right - left) / 2; // Tính chỉ số giữa
+            if (nums[mid] == target) { // Nếu phần tử giữa bằng target
+                return mid; // Trả về chỉ số giữa
+            } else if (nums[mid] < target) { // Nếu phần tử giữa nhỏ hơn target
+                left = mid + 1; // Di chuyển chỉ số trái sang phải
+            } else { // Nếu phần tử giữa lớn hơn target
+                right = mid - 1; // Di chuyển chỉ số phải sang trái
             }
         }
-
-        return left;
+        return left; // Nếu không tìm thấy, trả về chỉ số chèn
     }
 }
 // @lc code=end
