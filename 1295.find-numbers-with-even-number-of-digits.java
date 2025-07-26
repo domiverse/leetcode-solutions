5d6e649bba054acf4e155617640ca24fd2ffbd3e
Trung Thái <trungthai230700@gmail.com>
2025-07-26 14:08:22 +0700

    solved [1295] Find Numbers with Even Number of Digits

--- 1295.find-numbers-with-even-number-of-digits.java
+++ 1295.find-numbers-with-even-number-of-digits.java
@@ -7,22 +7,22 @@
 // @lc code=start
 class Solution {
     private boolean hasEvenDigits(int num) {
-    int demSoChan = 0;
-    while( num != 0){
-        demSoChan++;
-        num = num / 10;
-    }
-    return (demSoChan & 1) == 0;
-    }
+    int demSoChan = 0; // Biến để đếm số chữ số chẵn
+    while( num != 0){  // Duyệt qua từng chữ số của số num
+        demSoChan++; // Tăng biến đếm mỗi khi có một chữ số
+        num = num / 10; // Chia số num cho 10 để loại bỏ chữ số cuối cùng
+    } // Kiểm tra xem số chữ số chẵn có phải là số chẵn hay không
+    return (demSoChan & 1) == 0; // Sử dụng toán tử AND để kiểm tra tính chẵn lẻ của số chữ số
+    } // Hàm kiểm tra xem số có số chữ số chẵn hay không
 
-    public int findNumbers(int[] nums) {
-        int tongCacSoChan = 0;
-        for(int num : nums){
-            if(hasEvenDigits(num))
-            tongCacSoChan++;
-        } 
-        return tongCacSoChan;
-    }
-}
+    public int findNumbers(int[] nums) { // Hàm chính để tìm số lượng số có số chữ số chẵn
+        int tongCacSoChan = 0; // Biến để lưu trữ tổng số lượng số có số chữ số chẵn
+        for(int num : nums){ // Duyệt qua từng số trong mảng nums
+            if(hasEvenDigits(num)) // Kiểm tra xem số hiện tại có số chữ số chẵn hay không
+            tongCacSoChan++; // Nếu có, tăng biến đếm tổng số lượng số có số chữ số chẵn
+        }  //   Trả về tổng số lượng số có số chữ số chẵn
+        return tongCacSoChan; // Trả về tổng số lượng số có số chữ số chẵn
+    } // Hàm chính để tìm số lượng số có số chữ số chẵn
+} 
 // @lc code=end
 
