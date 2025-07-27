600d45bd0073111e3e41de12daa04527826522f1
Trung Thái <trungthai230700@gmail.com>
2025-07-27 17:16:18 +0700

    solved [1480] Running Sum of 1d Array

--- 1480.running-sum-of-1-d-array.java
+++ 1480.running-sum-of-1-d-array.java
@@ -10,10 +10,14 @@ import java.awt.font.NumericShaper;
 // @lc code=start
 class Solution {
     public int[] runningSum(int[] nums) {
-        for(int i = 1; i < nums.length; i++){
-            nums[i] = nums[i-1] + nums[i];
+        int[] result = new int[nums.length]; // Tạo mảng kết quả cùng kích thước với mảng nums
+        result[0] = nums[0]; // Gán giá trị đầu tiên của mảng kết quả bằng giá trị đầu tiên của mảng nums
+
+        for (int i = 1; i < nums.length; i++) { // Duyệt qua các phần tử từ chỉ số 1 đến cuối mảng
+            result[i] = result[i - 1] + nums[i]; // Cộng giá trị hiện tại với giá trị trước đó trong mảng kết quả
         }
-        return nums;
+
+        return result; // Trả về mảng kết quả
     }
 }
 // @lc code=end
