5c780e115b2cab6fe541d37ee0e641657ab73048
Trung Thái <trungthai230700@gmail.com>
2025-09-12 06:43:19 +0700

    solved Valid Palindrome

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -9,13 +9,18 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         if not s:  # Kiểm tra nếu chuỗi rỗng
             return True  # Trả về True nếu chuỗi rỗng
-        s = ''.join(c.lower() for c in s if c.isalnum())  # Lọc và chuyển đổi thành chữ thường
-        left, right = 0, len(s) - 1 # Khởi tạo hai con trỏ
-        while left < right: # Duyệt từ hai đầu chuỗi
-            if s[left] != s[right]: # So sánh ký tự hai đầu
-                return False # Trả về False nếu không khớp
-            left += 1 # Di chuyển con trỏ bên trái sang phải
-            right -= 1 # Di chuyển con trỏ bên phải sang trái
+        s = s.lower()  # Chuyển đổi chuỗi thành chữ thường
+        left, right = 0, len(s) - 1
+        while left < right:
+            if not s[left].isalnum():  # Bỏ qua ký tự không phải chữ cái hoặc số
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            if s[left] != s[right]:  # So sánh ký tự ở hai đầu
+                return False
+            left += 1
+            right -= 1
         return True # Trả về True nếu tất cả ký tự khớp
 # @lc code=end
-
