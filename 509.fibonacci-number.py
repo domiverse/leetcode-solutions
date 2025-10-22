354ca75f6e1484461872020d1ac8f1c1962aa7f4
Trung Thái <trungthai230700@gmail.com>
2025-10-23 05:15:30 +0700

    solved Fibonacci Number

--- 509.fibonacci-number.py
+++ 509.fibonacci-number.py
@@ -7,7 +7,7 @@
 # @lc code=start
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:  # Nếu n là 0 hoặc 1
+        if n <= 1:  # nếu n là 0 hoặc 1
             return n  # Trả về n
         return self.fib(n - 1) + self.fib(n - 2)  # Tính toán số Fibonacci
 # @lc code=end
