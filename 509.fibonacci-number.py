e31f353f52d60c9688a0d3a0e527ec513062fade
Trung Thái <trungthai230700@gmail.com>
2025-10-22 08:35:37 +0700

    solved Fibonacci Number

---
+++ 509.fibonacci-number.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=509 lang=python3
+#
+# [509] Fibonacci Number
+#
+
+# @lc code=start
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:  # Nếu n là 0 hoặc 1
+            return n  # Trả về n
+        return self.fib(n - 1) + self.fib(n - 2)  # Tính toán số Fibonacci
+# @lc code=end
+
