3b7eb9dda19771270ab97e6367d5a813730629a7
Trung Thái <trungthai230700@gmail.com>
2025-10-31 14:20:09 +0700

    solved Search Insert Position

---
+++ 35.search-insert-position - binary search.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=35 lang=python3
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return 0
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                right = middle -1
+        return left
+# @lc code=end
+
