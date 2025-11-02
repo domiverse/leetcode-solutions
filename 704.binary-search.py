8e1aa8fe397aa919d59c36fec13e0db52cc0a0d3
Trung Thái <trungthai230700@gmail.com>
2025-11-02 13:35:50 +0700

    solved Binary Search

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -8,16 +8,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if not nums:
-            return 0
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            middle = (left + right) // 2
+            return -1 
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            middle = (low + high) // 2
             if nums[middle] == target:
                 return middle
             elif nums[middle] < target:
-                 left = middle + 1
+                low = middle + 1
             else:
-                right = middle -1
+                high = middle -1
         return -1
+
 # @lc code=end
 
