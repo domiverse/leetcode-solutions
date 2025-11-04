317da79afc9eec5ea0428622332ce2c1ffc153ed
Trung Thái <trungthai230700@gmail.com>
2025-11-05 05:09:21 +0700

    solved Search a 2D Matrix

---
+++ 74.search-a-2-d-matrix.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        low, high = len(matrix), len(matrix[0])
+        left, right = 0, low*high - 1
+
+        while left <= right:
+            middle = left + (right - left) // 2
+            hang, cot = divmod(middle, high)
+            value = matrix[hang][cot]
+            if value == target:
+                return True
+            elif value < target:
+                left = middle + 1
+            else: 
+                right = middle - 1
+        return False
+
+# @lc code=end
+
