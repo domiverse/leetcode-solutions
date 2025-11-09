b15df72b5e5705f8419bf9c846d834ee8c6b7506
Trung Thái <trungthai230700@gmail.com>
2025-11-09 09:28:11 +0700

    solved Search a 2D Matrix

--- 69.sqrt-x.py
+++ 69.sqrt-x.py
@@ -9,16 +9,16 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
-        left, right = 2, x//2
-        while left <= right:
-            middle = left + (right - left) //2
+        low, high = 2, x//2
+        while low <= high:
+            middle = low + (high - low) // 2
             square = middle * middle
             if square == x:
                 return middle
             elif square < x:
-                left = middle + 1
+                low = middle + 1
             else:
-                right = middle - 1
-        return right
+                high = middle - 1
+        return high
 # @lc code=end
 
--- 74.search-a-2-d-matrix.py
+++ 74.search-a-2-d-matrix.py
@@ -9,22 +9,20 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not matrix or not matrix[0]:
             return False
-        soHang, soCot = len(matrix), len(matrix[0])
-        low, high = 0, soHang * soCot - 1
+        noR, noC = len(matrix), len(matrix[0])
+        low, high = 0, (noR * noC) - 1
         while low <= high:
             middle = low + (high - low) // 2
-            r, c = divmod(middle, soCot)
-            val = matrix[r][c]
-
-            if val == target:
+            row, col = divmod(middle, noC)
+            req = matrix[row][col]
+            if req == target:
                 return True
-            elif val < target:
+            elif req < target:
                 low = middle + 1
-            else:
+            else: 
                 high = middle - 1
         return False
 
 
 
 # @lc code=end
-
