2f729966fb46d347b3b60a03392ea82b5709cbd4
Trung Thái <trungthai230700@gmail.com>
2025-11-19 06:31:48 +0700

    solved Concatenation of Array

--- 1929.concatenation-of-array.py
+++ 1929.concatenation-of-array.py
@@ -7,7 +7,7 @@
 # @lc code=start
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        if not nums: #j
+        if not nums: #
             return [] 
         return nums + nums  
 # @lc code=end
